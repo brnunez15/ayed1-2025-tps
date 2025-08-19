d13db0cda19ed59d888f1da4df9a8631ba9683e2
brnunez15 <brnunez@uade.edu.ar>
2025-08-19 19:32:03 -0300

    Agregue funcion: diferencia_dias()

--- TP1/ej7.py
+++ TP1/ej7.py
@@ -39,6 +39,13 @@ def es_bisiesto (anio: int) -> bool:
      return (anio % 4 == 0 and anio % 100 != 0) or anio % 400 == 0
 
 def dia_siguiente (dia: int, mes: int, anio: int) -> tuple:
+    """
+    Esta funcion hace que una fecha ingresada la cambie al dia siguiente de una fecha valida.
+
+    Pre: La fecha ingresada, debe ser una fecha valida.
+
+    Post: devuelve una tupla de la fecha cambiada al dia siguiente.
+    """
 
     meses_31 = [1, 3, 5, 7, 8, 10, 12]
     meses_30 = [4, 6, 9, 11]
@@ -81,6 +88,13 @@ def dia_siguiente (dia: int, mes: int, anio: int) -> tuple:
     return dia, mes, anio
 
 def sumar_dias (dia: int, mes: int, anio: int, n: int) -> tuple:
+    """
+    Esta funcion suma una cierta cantidad de dias a una fecha correspondiente.
+
+    Pre: La fecha ingresada debe ser valida y la cantidad de dias a sumar debe ser positivo.
+
+    Post: Devuelve una tupla de la fecha ya con los dias sumados.
+    """
 
     for _ in range(n + 1):
         dia, mes, anio = dia_siguiente(dia, mes, anio)
@@ -98,9 +112,29 @@ def ingresar_fecha () -> int:
      anio = int(input("Ingrese el anio: "))
      return dia, mes, anio
 
+def diferencia_dias(d1, m1, a1, d2, m2, a2) -> int:
+    """
+    Genera una diferencia de dias entre dos fechas ingresadas.
 
-def main ():
+    Pre: Las fechas ingresadas deben ser validas.
+
+    Post: Devuelve un entero representando la cantidad de dias que hace la diferencia entre las fechas.
+    """
+    contador = 0
+
+    while (d1, m1, a1) != (d2, m2, a2):
+        d1, m1, a1 = dia_siguiente(d1, m1, a1)
+        contador += 1
+
+    return contador
+
+
+def main () -> None:
+    """
+    Funcion principal del programa.
+    """
     dia, mes, anio = ingresar_fecha()
+    
     if validar_fecha(dia, mes, anio) == True:
 
         dia_sig, mes_sig, anio_sig = dia_siguiente(dia, mes, anio)
@@ -110,9 +144,24 @@ def main ():
         dias_a_sumar = int(input("Ingrese la cantidad de dias a sumar: "))
         dia_nuevo, mes_nuevo, anio_nuevo = sumar_dias(dia, mes, anio, dias_a_sumar)
 
-        print(f"La fecha luego de agregar {dias_a_sumar} dia/s es: {dia_nuevo}/{mes_nuevo}/{anio_nuevo}")
+        print(f"La fecha luego de agregar {dias_a_sumar} dia/s es: {dia_nuevo}/{mes_nuevo}/{anio_nuevo}\n")
+
+        print("Ingresar dos fechas para saber la diferencia de dias entre ellas: \n")
+        dia1, mes1, anio1 = ingresar_fecha()
+        print(f"Fecha 1: {dia1}/{mes1}/{anio1}\n")
+
+        dia2, mes2, anio2 = ingresar_fecha()
+        print(f"Fecha 2: {dia2}/{mes2}/{anio2}\n")
+        
+        diferencia_de_dias = diferencia_dias(dia1,mes1,anio1,dia2,mes2,anio)
+        
+        if diferencia_de_dias > 0:
+            print(f"\nLa diferencia de dias entre {dia1}/{mes1}/{anio1} y {dia2}/{mes2}/{anio2} es de: {diferencia_de_dias} dias")
+        else:
+            print("Las fechas son iguales. No hay diferencia.")
     else:
         print("Debe ingresasar una fecha valida")
+
 
 if __name__ == "__main__":
     main()
