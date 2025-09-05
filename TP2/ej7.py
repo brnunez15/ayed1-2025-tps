72f41dad9b27ed66da56c2d54dccbcd549fef294
brnunez15 <brnunez@uade.edu.ar>
2025-09-05 19:36:11 -0300

    ej7: modificado y completo

--- TP2/ej7.py
+++ TP2/ej7.py
@@ -1,11 +1,37 @@
-lista1 = [8, 1, 3]
-lista2= [5, 9, 7]
+import random as rn
+from random import randint
+from typing import List
 
-def main():
-   lista1[1::2] = lista2
-   lista1[1::2].extend(lista2)
-   
-   print (lista1)   
+def generar_lista() -> List[int]:
+    """
+    Genera una lista de 5 numeros enteros aleatorios.
+
+    Post: Retorna la lista de enteros.
+    """
+    return [rn.randint(1,10) for _ in range (rn.randint(1,5))]
+
+def intercalar_listas(lista1: list[int], lista2: list[int]) -> List[int]:
+    """
+    Intercala elementos entre dos listas de enteros.
+
+    Pre: recibe una primer lista a la que se le intercalaran los elementos de la 2da lista recibida.
+
+    Post: Devuelve la primer lista con los elementos de la 2da lista intercalados.
+    """   
+    for y, dato in enumerate(lista2):
+        x = 2 * y + 1
+        lista1[x:x] = [dato]
+    
+    return lista1
+
+def main() -> None:
+    lista_1 = generar_lista()
+    lista_2 = generar_lista()
+    print(f"Lista 1: {lista_1}")
+    print(f"Lista 2: {lista_2}")
+
+    intercalar = intercalar_listas(lista_1, lista_2)
+    print(f"Listas intercaladas: {intercalar}")
 
 if __name__ == "__main__":
     main()
